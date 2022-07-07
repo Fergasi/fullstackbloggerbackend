9354793df29a4046e9668b6ff6a847f78e894db8
Fergasi <98353142+Fergasi@users.noreply.github.com>
2022-07-07 00:03:54 -0400

    commit 1

---
+++ routes/blogs.js
@@ -0,0 +1,19 @@
+const express = require("express");
+var router = express.Router();
+const { blogsDB } = require("../mongo");
+
+router.get("/hello-blogs", (req, res, next) => {
+  res.json({ message: "Hello from Express" });
+});
+
+router.get("/all-blogs", async function (req, res, next) {
+  try {
+    const collection = await blogsDB().collection("blogs50");
+    const blogs50 = await collection.find({}).toArray();
+    res.json(blogs50);
+  } catch (e) {
+    res.status(500).send("Error fetching posts:" + e);
+  }
+});
+
+module.exports = router;
