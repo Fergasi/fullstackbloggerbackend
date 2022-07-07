6949768475be5c214fc53214298eb74f084e5f64
Fergasi <98353142+Fergasi@users.noreply.github.com>
2022-07-07 18:06:20 -0400

    Completed with stretch goals

--- routes/blogs.js
+++ routes/blogs.js
@@ -9,7 +9,28 @@ router.get("/hello-blogs", (req, res, next) => {
 router.get("/all-blogs", async function (req, res, next) {
   try {
     const collection = await blogsDB().collection("blogs50");
-    const blogs50 = await collection.find({}).toArray();
+    const limit = Number(req.query.limit);
+    const skip = Number(req.query.limit) * (Number(req.query.page) - 1);
+    const sortField = req.query.sortField;
+    const sortOrder = req.query.sortOrder === "ASC" ? 1 : -1;
+    const filterField = req.query.filterField;
+    const filterValue = req.query.filterValue;
+
+    let filterObj = {};
+    if (filterField && filterValue) {
+      filterObj = { [filterField]: filterValue };
+    }
+    let sortObj = {};
+    if (sortField && sortOrder) {
+      sortObj = { [sortField]: sortOrder };
+    }
+    const blogs50 = await collection
+      .find(filterObj)
+      .sort(sortObj)
+      .limit(limit)
+      .skip(skip)
+      .toArray();
+
     res.send({ message: blogs50 });
   } catch (e) {
     res.status(500).send("Error fetching posts:" + e);
